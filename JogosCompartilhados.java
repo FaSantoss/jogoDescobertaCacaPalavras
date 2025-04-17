ef66bd4e213f6d7bd554cc85fccc5fe08833e285
FaSantoss <110779834+FaSantoss@users.noreply.github.com>
2025-04-16 21:28:03 -0300

    Update JogosCompartilhados.java

--- JogosCompartilhados.java
+++ JogosCompartilhados.java
@@ -1,34 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Jogo {
-    public Scanner scanner;
-    public Random random;
-    public int tentativas;
-
-    // Construtor classe base
-    public Jogo() {
-        scanner = new Scanner(System.in);
-        random = new Random();
-        tentativas = 0;
-    }
-    // dica progressiva
-    char dicaPorIndice(String palavra, int i) {
-        return palavra.charAt(i);
-    }
-    
-    // dica
-    public static char[] mostrarDica(String palavra) {
-        char[] dica = new char[2];
-        dica[0] = palavra.charAt(0);
-        dica[1] = palavra.charAt(palavra.length() - 1);
-        return dica;
-    }
-
-    public void iniciarJogo() {
-        
-    }
-}
 
 
 public class JogoDaDescoberta extends Jogo {
