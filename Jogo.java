ecb1098ec11829b5f15525008db6a1958f5d3ab7
FaSantoss <110779834+FaSantoss@users.noreply.github.com>
2025-05-07 02:37:30 -0300

    Update Jogo.java

--- Jogo.java
+++ Jogo.java
@@ -1,28 +1,11 @@
-public class Jogo {
-    public Scanner scanner;
-    public Random random;
-    public int tentativas;
+package com.projeto.jogos;
 
-    // Construtor classe base
-    public Jogo() {
-        scanner = new Scanner(System.in);
-        random = new Random();
-        tentativas = 0;
-    }
-    // dica progressiva
-    char dicaPorIndice(String palavra, int i) {
-        return palavra.charAt(i);
-    }
-    
-    // dica
-    public static char[] mostrarDica(String palavra) {
-        char[] dica = new char[2];
-        dica[0] = palavra.charAt(0);
-        dica[1] = palavra.charAt(palavra.length() - 1);
-        return dica;
-    }
+public abstract class Jogo {
 
-    public void iniciarJogo() {
-        
-    }
+        public abstract void iniciar(); // pode exibir tato o tabuleiro quanto a palavra embaralhda
+        public abstract boolean tentar(String palpite);
+        public abstract String dica();
+        public abstract void desistir();
+
 }
+// usei abstract para deixar "incompleto" e permitir que outras subclasses possam completar
