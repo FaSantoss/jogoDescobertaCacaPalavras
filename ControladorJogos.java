124e9ab14b0cb82a0b8ce9a3ac058fd3f834ead5
FaSantoss <110779834+FaSantoss@users.noreply.github.com>
2025-05-07 02:37:57 -0300

    Update ControladorJogos.java

--- ControladorJogos.java
+++ ControladorJogos.java
@@ -1,25 +1,44 @@
+package com.projeto.jogos;
+
 import java.util.Random;
-import java.util.Scanner;
 
-public class JogosCompartilhados {
-    public static void main(String[] args) {
-        
-        System.out.println("Escolha o jogo:");
-        System.out.println("1 - Jogo da Descoberta");
-        System.out.println("2 - Caça-Palavras");
-        System.out.print("Digite 1 ou 2: ");
-        int escolha = scanner.nextInt();
-
-        Jogo jogo;
-        if (escolha == 1) {
-            jogo = new JogoDaDescoberta();
-        } else if (escolha == 2) {
-            jogo = new CacaPalavras();
-        } else {
-            System.out.println("Opção inválida!");
-            return;
+// classe em comum
+public class JogoUtil {
+    //aqui eu checo os palpites e dou uma resposta se estiver certo ou errado, depende
+    public static boolean checarPalpite(String palpite, String palavraSecreta) {
+        if (palpite.equalsIgnoreCase(palavraSecreta)) {
+            System.out.println("Parabéns! Você acertou a palavra: " + palavraSecreta);
+            return true;
         }
+        System.out.println("Errado, tente novamente.");
+        return false;
+    }
 
-        jogo.iniciarJogo();
+// emaralha as palavras
+        public static String embaralharPalavra(String palavra) {
+        Random random = new Random();
+        char[] caracteres = palavra.toCharArray();
+        for (int i = caracteres.length - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            char aux = caracteres[i];
+            caracteres[i] = caracteres[j];
+            caracteres[j] = aux;
+        }
+        return new String(caracteres);
+    }
+
+    // manda msg de desistencia
+    public static void desistir(String palavraSecreta) {
+        System.out.println("Você desistiu! A palavra era: " + palavraSecreta);
+    }
+
+    //imprime o tabuleiro
+    public static void imprimirTabuleiro(char[][] tabuleiro) {
+        for (int i = 0; i < tabuleiro.length; i++) {
+            for (int j = 0; j < tabuleiro[i].length; j++) {
+                System.out.print(tabuleiro[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
